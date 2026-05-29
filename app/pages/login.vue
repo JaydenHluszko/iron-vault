2a44dcd20710bc8e52b4482bd5b1c14e0865d605
Jayden Hluszko <jayden.hluszko@gmail.com>
2026-05-29 00:51:33 -0400

    added login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,18 +1,52 @@
 <template>
-    <div class="card">
-        <Menubar :model="items" />
-    </div>
+    <UHeader title="Iron Vault" />
     <div class="card flex justify-center">
-        
+        <UContainer>
+            <UCard>
+                <template #header>
+                    Access the Iron Vault
+                </template>
+                <UInput placeholder="Password" v-model="password" type="password" />
+                <UButton label="Login" @click="handleLogin" />
+                <p v-if="errorMessage" class="text-red-500">{{ errorMessage }}</p>
+            </UCard>
+        </UContainer>
     </div>
 </template>
 
-<script setup>
-    import { ref } from "vue";
+<script setup lang="ts">
+    import { ref } from 'vue'
 
-    const items = ref([
-        {
-            label: 'Iron Vault',
+    // Define layout and bypass the global route guard logic for this page
+    definePageMeta({
+        middleware: []
+    })
+
+    const password = ref('');
+    const loading = ref(false);
+    const errorMessage = ref('');
+
+    async function handleLogin() {
+        if (!password.value) return;
+        loading.value = true;
+        errorMessage.value = '';
+
+        try {
+            const response = await $fetch('/api/auth/login', {
+                method: 'POST',
+                body: {
+                    password: password.value
+                }
+            });
+            if (response.success) {
+                await navigateTo('/');
+            } else {
+                errorMessage.value = response.message;
+            }
+        } catch (error: any) {
+            errorMessage.value = error.data?.message || 'Access Denied. Check master vault key.'
+        } finally {
+            loading.value = false;
         }
-    ]);
+    }
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,6 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: [
         '@nuxt/ui'
-    ]
+    ],
+    css: ['~/assets/css/main.css']
 })
